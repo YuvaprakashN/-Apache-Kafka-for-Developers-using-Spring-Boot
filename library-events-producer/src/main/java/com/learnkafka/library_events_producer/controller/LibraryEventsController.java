0c7f18f438cede0b8045449a5e03556b905f490f
Yuvaprakash N <yuvaprakashnv1999@gmail.com>
2025-08-21 19:14:38 +0530

    Add validation  to library Event domain classes

--- library-events-producer/src/main/java/com/learnkafka/library_events_producer/controller/LibraryEventsController.java
+++ library-events-producer/src/main/java/com/learnkafka/library_events_producer/controller/LibraryEventsController.java
@@ -2,7 +2,9 @@ package com.learnkafka.library_events_producer.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.learnkafka.library_events_producer.domain.LibraryEvent;
+import com.learnkafka.library_events_producer.domain.LibraryEventType;
 import com.learnkafka.library_events_producer.producer.LibraryEventProducer;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,8 +21,11 @@ public class LibraryEventsController {
     private LibraryEventProducer libraryEventProducer;
 
     @PostMapping("/v1/libraryevent")
-    public ResponseEntity<?> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
+    public ResponseEntity<?> postLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent) throws JsonProcessingException {
         log.info("postLibraryEvent Invoked");
+        if (LibraryEventType.NEW != libraryEvent.libraryEventType()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Only NEW event type is supported");
+        }
         libraryEventProducer.sendLibraryEventsWithProducerRecordAndCompletableFuture(libraryEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
