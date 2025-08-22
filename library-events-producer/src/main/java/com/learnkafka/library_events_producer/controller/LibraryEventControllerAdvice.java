9d16e3883cd1357b587f58e891a6ca31cd202f9b
Yuvaprakash N <yuvaprakashnv1999@gmail.com>
2025-08-22 05:46:22 +0530

    Add controllerAdvice for handling global exception

---
+++ library-events-producer/src/main/java/com/learnkafka/library_events_producer/controller/LibraryEventControllerAdvice.java
@@ -0,0 +1,7 @@
+package com.learnkafka.library_events_producer.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class LibraryEventControllerAdvice {
+}
