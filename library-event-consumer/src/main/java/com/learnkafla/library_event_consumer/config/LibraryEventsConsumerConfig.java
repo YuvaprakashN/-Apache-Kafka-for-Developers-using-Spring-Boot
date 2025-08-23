88b79e5eb1a8069f0c39298fe7cc1bda527cdb3c
Yuvaprakash N <yuvaprakashnv1999@gmail.com>
2025-08-23 14:58:02 +0530

    Consume msg using @KafkaListener

---
+++ library-event-consumer/src/main/java/com/learnkafla/library_event_consumer/config/LibraryEventsConsumerConfig.java
@@ -0,0 +1,12 @@
+package com.learnkafla.library_event_consumer.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+@Slf4j
+public class LibraryEventsConsumerConfig {
+
+}
---
+++ library-event-consumer/src/main/java/com/learnkafla/library_event_consumer/consumer/LibraryEventsConsumer.java
@@ -0,0 +1,19 @@
+package com.learnkafla.library_event_consumer.consumer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class LibraryEventsConsumer {
+
+    @KafkaListener(topics = "${spring.kafka.topics.library-events}", groupId = "spring.kafka.topics.library-events-group-id")
+    public void onMessage(ConsumerRecord<Integer, String> record) {
+        log.info("Consume Msg: {}", record);
+    }
+}
