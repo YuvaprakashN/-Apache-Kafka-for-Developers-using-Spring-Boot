67aa0ec8a27de6d97f042d56cb564ae0a60c2a09
Yuvaprakash N <yuvaprakashnv1999@gmail.com>
2025-08-23 07:54:35 +0530

    Consumer init

---
+++ library-event-consumer/src/test/java/com/learnkafla/library_event_consumer/LibraryEventConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnkafla.library_event_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
