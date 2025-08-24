3248f8d678ae2cda2eccb47cd2ca26343b9df1c9
Yuvaprakash N <yuvaprakashnv1999@gmail.com>
2025-08-24 10:00:43 +0530

    Error Handling, Retry: RetryScheduler to reprocess the msg

---
+++ library-event-consumer/src/main/java/com/learnkafla/library_event_consumer/scheduler/RetryScheduler.java
@@ -0,0 +1,51 @@
+package com.learnkafla.library_event_consumer.scheduler;
+
+import com.learnkafla.library_event_consumer.config.LibraryEventsConsumerConfig;
+import com.learnkafla.library_event_consumer.entity.FailureRecord;
+import com.learnkafla.library_event_consumer.jpa.FailureRecordRepository;
+import com.learnkafla.library_event_consumer.service.LibraryEventsService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+//@Component
+@Slf4j
+public class RetryScheduler {
+
+    @Autowired
+    LibraryEventsService libraryEventsService;
+
+    @Autowired
+    FailureRecordRepository failureRecordRepository;
+
+
+    @Scheduled(fixedRate = 10000 )
+    public void retryFailedRecords(){
+
+        log.info("Retrying Failed Records Started!");
+        var status = LibraryEventsConsumerConfig.RETRY;
+        failureRecordRepository.findAllByStatus(status)
+                .forEach(failureRecord -> {
+                    try {
+                        //libraryEventsService.processLibraryEvent();
+                        var consumerRecord = buildConsumerRecord(failureRecord);
+                        libraryEventsService.processLibraryEvent(consumerRecord);
+                        // libraryEventsConsumer.onMessage(consumerRecord); // This does not involve the recovery code for in the consumerConfig
+                        failureRecord.setStatus(LibraryEventsConsumerConfig.SUCCESS);
+                    } catch (Exception e){
+                        log.error("Exception in retryFailedRecords : ", e);
+                    }
+
+                });
+
+    }
+
+    private ConsumerRecord<Integer, String> buildConsumerRecord(FailureRecord failureRecord) {
+
+        return new ConsumerRecord<>(failureRecord.getTopic(),
+                failureRecord.getPartition(), failureRecord.getOffset_value(), failureRecord.getKey_value(),
+                failureRecord.getErrorRecord());
+
+    }
+}
